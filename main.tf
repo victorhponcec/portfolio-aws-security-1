3e2097df2a07e7bbc0fd90a91e78a9fa5feb5aed
victorhponcec <victorhponcec@gmail.com>
2025-09-24 18:02:53 -0300

    added: DB + NATGW + Subnets

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.111.0.0/16"
+  cidr_block           = "10.111.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
@@ -23,6 +23,17 @@ resource "aws_subnet" "public_a_az1" {
 resource "aws_subnet" "public_b_az2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.111.2.0/24"
+  availability_zone = var.az2
+}
+#NATGW
+resource "aws_subnet" "public_c_az1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.111.5.0/24"
+  availability_zone = var.az1
+}
+resource "aws_subnet" "public_d_az2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.111.6.0/24"
   availability_zone = var.az2
 }
 #App Tier
@@ -52,6 +63,24 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_eip" "ngw" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.igw]
+}
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public_c_az1.id
+  depends_on    = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private_nat_rtb" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+}
+
 resource "aws_route_table" "public_rtb" {
   vpc_id = aws_vpc.main.id
   route {
@@ -60,7 +89,7 @@ resource "aws_route_table" "public_rtb" {
   }
 }
 
-resource "aws_route_table" "private_app_rtb" {
+resource "aws_route_table" "private_db_rtb" {
   vpc_id = aws_vpc.main.id
 }
 
@@ -72,14 +101,31 @@ resource "aws_route_table_association" "public_b_az2" {
   subnet_id      = aws_subnet.public_b_az2.id
   route_table_id = aws_route_table.public_rtb.id
 }
+resource "aws_route_table_association" "public_c_az1" {
+  subnet_id      = aws_subnet.public_c_az1.id
+  route_table_id = aws_route_table.public_rtb.id
+}
+resource "aws_route_table_association" "public_d_az2" {
+  subnet_id      = aws_subnet.public_d_az2.id
+  route_table_id = aws_route_table.public_rtb.id
+}
 resource "aws_route_table_association" "private_a_az1" {
   subnet_id      = aws_subnet.private_a_az1.id
-  route_table_id = aws_route_table.private_app_rtb.id
+  route_table_id = aws_route_table.private_nat_rtb.id
 }
 resource "aws_route_table_association" "private_b_az2" {
   subnet_id      = aws_subnet.private_b_az2.id
-  route_table_id = aws_route_table.private_app_rtb.id
+  route_table_id = aws_route_table.private_nat_rtb.id
+}
+resource "aws_route_table_association" "private_c_az1" {
+  subnet_id      = aws_subnet.private_c_az1.id
+  route_table_id = aws_route_table.private_db_rtb.id
+}
+resource "aws_route_table_association" "private_d_az2" {
+  subnet_id      = aws_subnet.private_d_az2.id
+  route_table_id = aws_route_table.private_db_rtb.id
 }
 #to do: 
-#+rtb association db subnet
-#+add nat to allow app subnet internet access | create 2 subnets (one in each az) to place NAT
+#+DBs
+#+Secrets manager + endpoint | add db's password secret
+#+VPC Flow logs + bucket
--- vpc_endpoints.tf
+++ vpc_endpoints.tf
@@ -3,10 +3,7 @@ resource "aws_vpc_endpoint" "ssm" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.${var.region1}.ssm"
   vpc_endpoint_type = "Interface"
-  #added endpoint to public subnet so public subnets can read private DNS of SSM
   subnet_ids = [
-    #aws_subnet.public_a_az1.id,
-    #aws_subnet.public_b_az2.id
     aws_subnet.private_a_az1.id,
     aws_subnet.private_b_az2.id
   ]
@@ -19,8 +16,6 @@ resource "aws_vpc_endpoint" "ec2messages" {
   service_name      = "com.amazonaws.${var.region1}.ec2messages"
   vpc_endpoint_type = "Interface"
   subnet_ids = [
-    #aws_subnet.public_a_az1.id,
-    #aws_subnet.public_b_az2.id
     aws_subnet.private_a_az1.id,
     aws_subnet.private_b_az2.id
   ]
@@ -33,8 +28,6 @@ resource "aws_vpc_endpoint" "ssmmessages" {
   service_name      = "com.amazonaws.${var.region1}.ssmmessages"
   vpc_endpoint_type = "Interface"
   subnet_ids = [
-    #aws_subnet.public_a_az1.id,
-    #aws_subnet.public_b_az2.id
     aws_subnet.private_a_az1.id,
     aws_subnet.private_b_az2.id
   ]
